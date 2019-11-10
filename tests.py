892b0c851edc478d674f742acf2503426f8134b8
ggolish <ggolish@sycamores.indstate.edu>
2019-11-09 19:05:30 -0500

    Implemented test for power method.

--- tests.py
+++ tests.py
@@ -7,10 +7,22 @@ import os
 
 def load_test(number):
     path = os.path.join("data", "test{:02d}".format(number))
-    return eigen.read_matrix(path)
+    with open(path, "r") as fd:
+        return eigen.read_matrix(fd)
+
+def plot_testcase(number, power_data):
+    title = "Test Case {:02d}".format(number)
+    plt.plot(power_data)
+    plt.ylabel("Largest Eigenvalue")
+    plt.xlabel("Iteration")
+    plt.title(f"Power Method: {title}")
+    plt.show()
 
 def main(args):
-    pass
+    A = load_test(args.number)
+    iterations = args.iterations if args.iterations != None else 100
+    eigenval, eigenvec, power_data = eigen.power_method(A, iterations=iterations)
+    plot_testcase(args.number, power_data)
 
 if __name__ == "__main__":
     import argparse
@@ -23,6 +35,8 @@ if __name__ == "__main__":
     )
 
     parser.add_argument("number", type=int, help="The test case number")
+    parser.add_argument("-i", "--iterations", type=int, help="Number of iterations to run [default 1000]")
+
     args = parser.parse_args(sys.argv[1:])
 
     main(args)
