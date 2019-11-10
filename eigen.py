09abff9ef17235566cb5ed0d6eeaf595b2cc0ee0
ggolish <ggolish@sycamores.indstate.edu>
2019-11-09 22:29:34 -0500

    Refactored power method to be a generator function.

--- eigen.py
+++ eigen.py
@@ -5,25 +5,15 @@ import sys
 
 # Computes the largest magnitude eigenvalue of A via the power method, within
 # a tolerance of delta or for iterations iterations
-def power_method(A, delta=0.0001, iterations=None):
+def power_method(A):
     utils.validate_matrix(A)
     x = np.zeros(A.shape[0])
     x[0] = 1.0
-    pn = float("inf")
-    iteration = 0
-    data = []
     while True:
         p = A @ x
         n = np.max(np.abs(p))
-        data.append(n)
         x = (1.0 / n) * p
-
-        if (not iterations and abs(n - pn) < delta) or (iterations and iteration >= iterations):
-            break
-
-        pn = n
-        iteration += 1
-    return (n, x, data)
+        yield n
 
 def jacobi_method(A, delta=0.0001):
     return (0, np.array())
@@ -33,32 +23,14 @@ def read_matrix(fd):
     return np.array(x)
 
 def main(args):
-    infd = sys.stdin
-    outfd = sys.stdout
-
-    if args.f:
-        try:
-            infd = open(args.f, "r")
-        except:
-            utils.die(f"Unable to open {args.f} for reading")
-
-    if args.o:
-        try:
-            outfd = open(args.o, "w")
-        except:
-            utils.die(f"Unable to open {args.o} for writing")
-
-    A = read_matrix(infd)
-    infd.close()
-
-    if args.jacobi:
-        n, v = jacobi_method(A)
-    else:
-        n, v, _ = power_method(A)
-
-    outfd.write(f"lambda = {repr(n)}\n")
-    outfd.write(f"v = {repr(v.tolist())}\n")
-    outfd.close()
+    A = read_matrix(sys.stdin)
+    method = jacobi_method if args.jacobi else power_method
+    prev_eigenval = float("inf")
+    for eigenval in method(A):
+        if abs(eigenval - prev_eigenval) < 0.00001:
+            break
+        prev_eigenval = eigenval
+    print("{:0.4f}".format(eigenval))
 
 if __name__ == "__main__":
     import argparse
@@ -66,13 +38,11 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(
             prog="Eigenvalue Approximator", 
             description="Numerically approximates the eigenvalues of an input matrix",
-            usage=f"{sys.argv[0]} [--power | --jacobi] [-f <path-to-file>] [-o <output-path>]"
+            usage=f"{sys.argv[0]} [--power | --jacobi]"
     )
 
     parser.add_argument("--power", action="store_true", help="Use the power method [default]")
     parser.add_argument("--jacobi", action="store_true", help="Use the jacobi method")
-    parser.add_argument("-f", type=str, help="Read input matrix from file")
-    parser.add_argument("-o", type=str, help="Dump output into file")
     args = parser.parse_args(sys.argv[1:])
 
     main(args)
